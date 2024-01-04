3653c49658b3d40dd3c81ad579806ba1625e1589
ImIrys <irys.lda@gmail.com>
2024-01-04 00:52:53 -0300

    Commit inicial.

---
+++ src/aula_001/Aula001.java
@@ -0,0 +1,9 @@
+package aula_001;
+
+public class Aula001 {
+    public int irysAge = 31;
+
+    public int getIrysAge(){
+        return irysAge;
+    }
+}
---
+++ src/aula_001/Aula001Test.java
@@ -0,0 +1,8 @@
+package aula_001;
+
+public class Aula001Test {
+    public static void main(String[] args) {
+        Aula001 aula001 = new Aula001();
+        System.out.println(aula001.getIrysAge());
+    }
+}
